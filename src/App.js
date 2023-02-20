80fd3d4f5552b6744eb1b647ed15e47ca32ea377
Gokcenomer <b.ylmz55@hotmail.com>
2023-02-21 00:05:26 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { characters as initialCharacters } from "./Characters";
+import { useState } from "react";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [characters, setCharacters] = useState(initialCharacters);
+
+  const shuffleArray = () => {
+    setCharacters([...characters.sort((a, b) => 0.5 - Math.random())]);
+  };
+  const handleIsClicked = (character) => {
+    character.isClicked = true;
+  };
+  const updateScore = (character) => {
+    if (character.isClicked) {
+      setScore(0);
+      clearState();
+      updateBestScore();
+    } else {
+      setScore(score + 1);
+    }
+  };
+  const updateBestScore = () => {
+    console.log(score);
+    if (score >= bestScore) {
+      setBestScore(score);
+    }
+  };
+  const clearState = () => {
+    setCharacters(
+      initialCharacters.map((character) => ({ ...character, isClicked: false }))
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="header yellow-text">Memory Game </h1>
+      <p className="info yellow-text">
+        Get points by clicking on image but dont click on any more than once
+      </p>
+      <p className="score yellow-text">Score: {score}</p>
+      <p className="best-score yellow-text">Best Score : {bestScore}</p>
+      <div className="card-container">
+        {characters.map((character) => (
+          <div
+            key={character.name}
+            className="card"
+            onClick={() => {
+              shuffleArray();
+              updateScore(character);
+              handleIsClicked(character);
+            }}
+          >
+            <img
+              className="card-image"
+              src={character.imagePath}
+              alt={character.name}
+            ></img>
+            <p className="card-text"> {character.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Characters.js
@@ -0,0 +1,50 @@
+import arwenImagePath from "./img/arwen.jpg";
+import bilboImagePath from "./img/bilbo.jpg";
+import eowinImagePath from "./img/eowin.jpg";
+import frodoImagePath from "./img/frodo.jpg";
+import gollumImagePath from "./img/gollum.jpg";
+import legolasImagePath from "./img/legolas.jpg";
+import pippinImagePath from "./img/pippin.jpg";
+import galadrielImagePath from "./img/galadriel.jpg";
+export let characters = [
+  {
+    name: "Arwen",
+    imagePath: arwenImagePath,
+    isClicked: false,
+  },
+  {
+    name: "Bilbo",
+    imagePath: bilboImagePath,
+    isClicked: false,
+  },
+  {
+    name: "Eowin",
+    imagePath: eowinImagePath,
+    isClicked: false,
+  },
+  {
+    name: "Frodo",
+    imagePath: frodoImagePath,
+    isClicked: false,
+  },
+  {
+    name: "Gollum",
+    imagePath: gollumImagePath,
+    isClicked: false,
+  },
+  {
+    name: "Legolas",
+    imagePath: legolasImagePath,
+    isClicked: false,
+  },
+  {
+    name: "Pippin",
+    imagePath: pippinImagePath,
+    isClicked: false,
+  },
+  {
+    name: "Galadriel",
+    imagePath: galadrielImagePath,
+    isClicked: false,
+  },
+];
